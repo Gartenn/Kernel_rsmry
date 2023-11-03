fcca9e4df7f32cd4b9eb406838fc8aeb41c22325
Garten <brilliangarten@gmail.com>
2023-11-03 21:14:42 +0700

    Update rose.sh

--- rose.sh
+++ rose.sh
@@ -9,8 +9,6 @@ export ARCH=arm64
 export KBUILD_BUILD_HOST=GearCI
 export KBUILD_BUILD_USER="Gartenn"
 git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git clang
-git clone --depth=1 https://gitlab.com/firefly-linux/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.git los-4.9-64
-git clone --depth=1 https://github.com/MayuriLabs/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
 
 make O=out ARCH=arm64 rosemary_defconfig
 
@@ -19,8 +17,6 @@ make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC="clang" \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
 }
 
