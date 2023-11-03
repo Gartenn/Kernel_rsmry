8f1d9bafdf23cfb8f58c099f6c660173a984c39e
Garten <brilliangarten@gmail.com>
2023-11-03 23:36:27 +0700

    Add files via upload

---
+++ rose.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+function compile() 
+{
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 120G
+export ARCH=arm64
+export KBUILD_BUILD_HOST=GearCI
+export KBUILD_BUILD_USER="Gartenn"
+git clone --depth=1 https://github.com/EmanuelCN/zyc_clang-14 clang
+
+make O=out ARCH=arm64 rosemary_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+
+function zupload()
+{
+git clone --depth=1 https://github.com/Gartenn/Anykernel3.git -b moon AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+zip -r9 Drgnprjktrosemary.zip *
+}
+
+compile
+zupload
