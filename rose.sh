71ec00730841c057127151d4982db17620c990b6
Garten <brilliangarten@gmail.com>
2023-11-04 00:46:33 +0700

    Add files via upload

---
+++ rose.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function compile() 
+{
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 120G
+export ARCH=arm64
+export KBUILD_BUILD_HOST=GearCI
+export KBUILD_BUILD_USER="Gartenn"
+git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git clang
+git clone --depth=1 https://gitlab.com/firefly-linux/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.git los-4.9-64
+git clone --depth=1 https://github.com/MayuriLabs/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
+
+make O=out ARCH=arm64 rosemary_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+
+function zupload()
+{
+git clone --depth=1 https://github.com/Gartenn/Anykernel3.git -b moon AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+zip -r9 Drgnprjkt-rosemary.zip *
+}
+
+compile
+zupload
