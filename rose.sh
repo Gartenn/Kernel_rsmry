a52bc3508cb6a0bb7cf7079d6f19d573ed5b3f76
Garten <brilliangarten@gmail.com>
2023-11-04 01:21:10 +0700

    Update rose.sh

--- rose.sh
+++ rose.sh
@@ -16,8 +16,8 @@ PATH="${PWD}/clang/bin:${PATH}
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC="clang" \
-                      CROSS_COMPILE=aarch64-linux-gnu-
-                      CROSS_COMPILE_ARM32=arm-linux-gnueabihf-
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabihf- \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
 }
 
